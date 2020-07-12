47e70cbe93cb1eeb6479a4d07b1dbc01025c50ce
Joao Souza Moura <js.moura04@gmail.com>
2020-07-12 19:39:02 -0300

    Adicionar arquivos de projeto.

---
+++ RallyDakar.Dominio/Entidades/Temporada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Temporada
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime? DataFim { get; set; }
+
+        public ICollection<Equipe> Equipes { get; set; }
+    }
+}
